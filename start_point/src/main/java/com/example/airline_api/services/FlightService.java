c1247a9f25c8d614942f4e4ef7c5808162333bf8
Emily Reid <emily.reid31@gmail.com>
2023-11-07 17:46:35 +0000

    updated PassengerService with getAllPassengers and getPassengersById

--- start_point/src/main/java/com/example/airline_api/services/FlightService.java
+++ start_point/src/main/java/com/example/airline_api/services/FlightService.java
@@ -9,4 +9,5 @@ public class FlightService {
 
     @Autowired
     FlightRepository flightRepository;
+
 }
--- start_point/src/main/java/com/example/airline_api/services/PassengerService.java
+++ start_point/src/main/java/com/example/airline_api/services/PassengerService.java
@@ -1,13 +1,25 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PassengerService {
 
     @Autowired
     PassengerRepository passengerRepository;
+
+    public List<Passenger> getAllPassengers(){
+        return passengerRepository.findAll();
+    }
+
+    public Optional<Passenger> getPassengersById(Long passengerId){
+        return passengerRepository.findById(passengerId);
+    }
 
 }
