a1664cabfd4ee2db3bdac9a9372381350846b829
Emily Reid <emily.reid31@gmail.com>
2023-11-08 15:29:47 +0000

    created initial BookingDTO

--- start_point/src/main/java/com/example/airline_api/models/BookingDTO.java
+++ start_point/src/main/java/com/example/airline_api/models/BookingDTO.java
@@ -3,9 +3,6 @@ package com.example.airline_api.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
 public class BookingDTO {
-
-    @Id
-    private long id;
+    
 }
